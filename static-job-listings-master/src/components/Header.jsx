2720ed756f2e0c418872ca4583bc902e683d8f16
Willian Sato <willianmassayukisato@gmail.com>
2023-06-23 13:12:31 -0300

    Trabalhando no header

---
+++ static-job-listings-master/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import '../styles/components/header.scss';
+import Background from '../images/bg-header-desktop.svg'
+
+const Header = () => {
+  return (
+    <div class="header">
+        {/*Colocar a imagem de fundo aqui*/}      
+    </div>
+  )
+}
+
+export default Header
